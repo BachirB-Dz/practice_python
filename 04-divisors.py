0a31cc7ee82dfd0262d2a31757d005b6b265261b
BachirB-Dz <72015846+BachirB-Dz@users.noreply.github.com>
2020-11-08 11:48:04 +0100

    Update 04-divisors.py

--- 04-divisors.py
+++ 04-divisors.py
@@ -1,5 +1,4 @@
 # Creating a program that asks the user for a number and then prints out a list of all the divisors of that number.
-# I know the program could be a lot shorter, but I wanted to handle all the possibilities that I could think of.
 
 
 def divisors():
@@ -17,16 +16,14 @@ def divisors():
         if divisorList == [1,number]:
             print('Uh-oh, you\'ve got a prime number there.')
         
-        decision = input('Write "y" or "yes" to continue or anything else to quit: ')
-        
-        if decision.lower() in ['y', 'yes']:
+        if input('Write "y" or "yes" to continue or anything else to quit: ').lower() in ['y', 'yes']:
             divisors()
         else:
             pass
-    
+
     except ValueError:
         print('Your input is invalid.')
         divisors()
 
-        
+
 divisors()
