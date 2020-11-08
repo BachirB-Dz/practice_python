e29309dcb0cb70976b83c3589518930a6cc58111
BachirB-Dz <72015846+BachirB-Dz@users.noreply.github.com>
2020-11-08 11:23:45 +0100

    Update 04-divisors.py

--- 04-divisors.py
+++ 04-divisors.py
@@ -3,36 +3,29 @@
 
 
 def divisors():
-    # If the input is neither an integer nor a float, the user is notified and asked to continue or not.
-    # Otherwise, we have three possibilities: zero, a positive integer, or a negative integer.
     try:
         number = int(input('Write a number to get its divisors: '))
-        divisors =  []
+        divisorList =  []
         if number == 0:
             print('All non-zero numbers are divisors of 0.')
-        else:
+        elif number != 0:
             for n in range(1,abs(number) + 1):
                 if number % n == 0:
-                    divisors.append(n)
-            print(f'The divisors of {number} are: ' + str(divisors).replace('[','').replace(']',''))
+                    divisorList.append(n)
+            print(f'The divisors of {number} are: ' + str(divisorList).replace('[','').replace(']',''))
         
-        if divisors == [1,number]:
+        if divisorList == [1,number]:
             print('Uh-oh, you\'ve got a prime number there.')
+        
+        decision = input('Write "y" or "yes" to continue or anything else to quit: ')
+        
+        if decision.lower() in ['y', 'yes']:
+            divisors()
+        else:
+            pass
     
     except ValueError:
         print('Your input is invalid.')
 
 
-def another():
-    # The user is given the choice to continue with another number or finish the program.
-    # I tried using a while loop, but without an input from the user, the loop is infinite, necessitating the use of ctrl+c to stop the program.
-    decision = input('Write "y" or "yes" to continue or anything else to quit: ')
-    
-    if decision.lower() in ['y', 'yes']:
-        divisors()
-        another()
-    else:
-        pass
-
 divisors()
-another()
